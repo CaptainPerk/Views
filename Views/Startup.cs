10427cc6639ac623838a0ac7b0b8faf1b5d4a4bf
Jon Perkins <captainperk81@gmail.com>
2018-11-05 07:11:03 -0500

    Registering a Custom View Engine

--- Views/Startup.cs
+++ Views/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Views.Infrastructure;
 
 namespace Views
 {
@@ -9,6 +11,7 @@ namespace Views
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.Configure<MvcViewOptions>(o => o.ViewEngines.Insert(0, new DebugDataViewEngine()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
