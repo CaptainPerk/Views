776972a2860bebcc2cd2a50f1ddf71880df9d2ed
Jon Perkins <captainperk81@gmail.com>
2018-11-06 07:25:19 -0500

    Using Partial Views

--- Views/Startup.cs
+++ Views/Startup.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
-using Views.Infrastructure;
 
 namespace Views
 {
